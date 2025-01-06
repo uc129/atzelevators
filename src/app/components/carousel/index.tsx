e5cd03cd3965256704447f42b2d08dd791d3d909
Utkarsh Chaudhary <38369815+uc129@users.noreply.github.com>
2025-01-06 12:57:40 +0530

    variants

--- src/app/components/carousel/index.tsx
+++ src/app/components/carousel/index.tsx
@@ -5,12 +5,14 @@ import { ProductsDetail } from '@/app/constants/products'
 
 const OPTIONS: EmblaOptionsType = {}
 const SLIDE_COUNT = ProductsDetail.length
+const slides = ProductsDetail.flatMap((product) => product.variants)
+
 const SLIDES = ProductsDetail
 
 const EmblaCarouselComponent: React.FC = () => (
     <>
         <h1> Embla Carousel Header </h1>
-        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+        <EmblaCarousel slides={slides} options={OPTIONS} />
         <h1>Carousel Footer</h1>
     </>
 )
